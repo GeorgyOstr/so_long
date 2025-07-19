71fe08586998fd11effe3b3c2b7e5fa712aec1d9
georgy <georgyostr@gmail.com>
2025-07-20 01:16:19 +0900

    norm

--- loader.c
+++ loader.c
@@ -37,6 +37,35 @@ int	load_assets(t_game *game)
 	}
 }
 
+void	data_switch(t_map *map, char *line, int i, int linenum)
+{
+	if (line[i] == '1')
+		map->data[linenum * map->dim.x + i] = 0;
+	else if (line[i] == '0')
+		map->data[linenum * map->dim.x + i] = 1;
+	else if (line[i] == 'C')
+	{
+		map->points_to_finish += 1;
+		map->data[linenum * map->dim.x + i] = 2;
+	}
+	else if (line[i] == 'E')
+	{
+		map->data[linenum * map->dim.x + i] = 3;
+		if (map->exit.x >= 0)
+			error_map_check();
+		map->exit = (t_pos){i, linenum};
+	}
+	else if (line[i] == 'P')
+	{
+		map->data[linenum * map->dim.x + i] = 1;
+		if (map->p.x >= 0)
+			error_map_check();
+		map->p = (t_pos){i, linenum};
+	}
+	else
+		error_map_check();
+}
+
 int	process_line(t_map *map, int linenum, char *line)
 {
 	int			i;
@@ -48,34 +77,7 @@ int	process_line(t_map *map, int linenum, char *line)
 	if (map->dim.x > MAX_WIDTH || len != map->dim.x)
 		error_map_check();
 	while (line[i] && line[i] != '\n')
-	{
-		if (line[i] == '1')
-			map->data[linenum * map->dim.x + i] = 0;
-		else if (line[i] == '0')
-			map->data[linenum * map->dim.x + i] = 1;
-		else if (line[i] == 'C')
-		{
-			map->points_to_finish += 1;
-			map->data[linenum * map->dim.x + i] = 2;
-		}
-		else if (line[i] == 'E')
-		{
-			map->data[linenum * map->dim.x + i] = 3;
-			if (map->exit.x >= 0)
-				error_map_check();
-			map->exit = (t_pos){i, linenum};
-		}
-		else if (line[i] == 'P')
-		{
-			map->data[linenum * map->dim.x + i] = 1;
-			if (map->p.x >= 0)
-				error_map_check();
-			map->p = (t_pos){i, linenum};
-		}
-		else
-			error_map_check();
-		i++;
-	}
+		data_switch(map, line, i++, linenum);
 	return (1);
 }
 
@@ -84,7 +86,6 @@ int	load_map(t_map *map, char *map_name)
 	char		*s;
 	const int	fd = open(map_name, O_RDONLY);
 	int			i;
-	int			res;
 
 	map->points_to_finish = 0;
 	map->steps = 0;
@@ -97,15 +98,14 @@ int	load_map(t_map *map, char *map_name)
 	if (fd < 0)
 		error_file_exist();
 	s = get_next_line(fd);
-	res = 1;
-	while (i < MAX_HEIGHT && s && res)
+	while (i < MAX_HEIGHT && s)
 	{
-		res = process_line(map, i++, s);
+		process_line(map, i++, s);
 		free(s);
 		s = get_next_line(fd);
 	}
 	close(fd);
 	map->dim.y = i;
-	if (get_next_line(fd) || !res || map->p.x < 0 || map->exit.x < 0)
+	if (get_next_line(fd) || map->p.x < 0 || map->exit.x < 0)
 		error_map_check();
 }
